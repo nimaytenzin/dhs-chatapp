af5abbadd4df652773e5522d292638cdce22917e
nimaytenzin <nimaytenzin@gmail.com>
2020-11-10 13:30:05 +0600

    changed sender name error

--- public/js/main.js
+++ public/js/main.js
@@ -36,7 +36,7 @@ chatForm.addEventListener('submit', e =>{
 function outputMessage (message){
     const div = document.createElement('div');
     div.classList.add('message');
-    div.innerHTML =`<p class="meta">${username} <span>${message.time}</span></p>
+    div.innerHTML =`<p class="meta">${message.username} <span>${message.time}</span></p>
     <p class="text">
         ${message.text}
     </p>`;
